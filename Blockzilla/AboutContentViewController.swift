90ab1a477d160a0e62bba00068706cf08dc75abc
Bryan Munar <brymunar@gmail.com>
2017-10-25 15:42:55 -0700

    loading indicator for support webpages #575

--- Blockzilla/AboutContentViewController.swift
+++ Blockzilla/AboutContentViewController.swift
@@ -7,12 +7,11 @@ import SnapKit
 import UIKit
 import WebKit
 
-class AboutContentViewController: UIViewController, UIWebViewDelegate {
-    private let url: URL
+class AboutContentViewController: SettingsContentViewController, UIWebViewDelegate {
 
     init(url: URL) {
+        super.init()
         self.url = url
-        super.init(nibName: nil, bundle: nil)
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -43,21 +42,5 @@ class AboutContentViewController: UIViewController, UIWebViewDelegate {
 
     override var preferredStatusBarStyle : UIStatusBarStyle {
         return UIStatusBarStyle.lightContent
-    }
-
-    func webViewDidFinishLoad(_ webView: UIWebView) {
-        revealWebView(webView)
-    }
-
-    func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
-        revealWebView(webView)
-    }
-
-    private func revealWebView(_ webView: UIWebView) {
-        // Add a small delay to allow the stylesheets to load and avoid flicker.
-        let delayTime = DispatchTime.now() + Double(Int64(200 * Double(NSEC_PER_MSEC))) / Double(NSEC_PER_SEC)
-        DispatchQueue.main.asyncAfter(deadline: delayTime) {
-            webView.animateHidden(false, duration: 0.3)
-        }
     }
 }
